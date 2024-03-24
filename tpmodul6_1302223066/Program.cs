65e75c261c2534bffe32fd4dc57b1139f69a958b
rsam-66 <samuelriyanto57@gmail.com>
2024-03-25 01:30:37 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223066/Program.cs
+++ tpmodul6_1302223066/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - Reimark Samuel");
+        for (int i = 0; i < 10000000; i++)
+        {
+            video.IncreasePlayCount(1);
+        }
+
+        video.PrintVideoDetails();
+    }
+}
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        this.id = RandomNumber();
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    private int RandomNumber()
+    {
+        Random random = new Random();
+        return random.Next(00000, 99999);
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount = count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID    : " + this.id);
+        Console.WriteLine("Title : " + this.title);
+        Console.WriteLine("Play Count : " + this.playCount);
+    }
+}
--- tpmodul6_1302223066/obj/Debug/net8.0/tpmodul6_1302223066.AssemblyInfo.cs
+++ tpmodul6_1302223066/obj/Debug/net8.0/tpmodul6_1302223066.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_1302223066")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1267c2cd3b4b6cc2ba7c1d4ff6203453f810e91e")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_1302223066")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_1302223066")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
